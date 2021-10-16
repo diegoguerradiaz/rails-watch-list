6663c945bc275b61305e5a0be4fe19c4ab3e55a4
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-16 16:02:41 -0300

    addes some internal notes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,23 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @lists
+  end
 end
+
+# def create
+#     @restaurant = Restaurant.new(restaurant_params)
+
+#     if @restaurant.save
+#       redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+#     else
+#       render :new
+#     end
+#   end
