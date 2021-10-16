ccdb721e4a734217846b883755e078dc52ccb51a
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-16 15:35:42 -0300

    created routes for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  get 'lists', to: 'lists#index' # index
+  get 'lists/:id', to: 'lists#show' # show
+  get 'lists/new', to: 'lists#new' # new (form)
+  post 'lists', to: 'lists/create' # new (create the list)
 end
